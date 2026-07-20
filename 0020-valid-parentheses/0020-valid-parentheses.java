353a79b3d5696f1d3a86761f6e94a289d10361ec
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-07-20 22:10:21 +0530

    Time: 4 ms (38.07%), Space: 43.1 MB (73.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } 
+            else if (ch == ')' || ch == '}' || ch == ']') {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                if ((ch == ')' && st.peek() == '(') ||
+                    (ch == '}' && st.peek() == '{') ||
+                    (ch == ']' && st.peek() == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (st.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
