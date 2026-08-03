69ca11a4072ae9b5c04115d6d07a186189897cd1
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-03 12:35:24 +0530

    Time: 1 ms (61.32%), Space: 42.6 MB (99.51%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(strs);
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
-        int index = 0;
+        int index = 0;                                                      
         while(index < s1.length() && index < s2.length()){
             if(s1.charAt(index) == s2.charAt(index)){
                 index++;
