cb864290d586782544d7c7b42b98ef16559acb02
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-26 23:06:49 +0530

    Time: 0 ms (100%), Space: 43 MB (19.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,25 @@
 class Solution {
-    private int[] memo;
+    private  int recursion(int[] nums, int index, int[] memo){
+        // base case
+        if(index >= nums.length){
+            return  0 ;
+        }
+        if(memo[index] != -1){
+            return memo[index];
+        }
+        int include = nums[index] + recursion(nums , index + 2 , memo);
+        int exclude = 0 + recursion(nums, index +1 , memo);
 
+        memo[index] =  Math.max(include, exclude);
+        return memo[index];
+
+    }
     public int rob(int[] nums) {
-        memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return recursion(0, nums);
+        int index = 0;
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo,-1);
+        int answer = recursion(nums , index , memo);
+        return answer ;
     }
 
-    private int recursion(int i, int[] nums) {
-        if (i >= nums.length) {
-            return 0;
-        }
-
-        if (memo[i] != -1) {
-            return memo[i];
-        }
-
-        int rob = nums[i] + recursion(i + 2, nums);
-        int skip = recursion(i + 1, nums);
-
-        return memo[i] = Math.max(rob, skip);
-    }
 }
