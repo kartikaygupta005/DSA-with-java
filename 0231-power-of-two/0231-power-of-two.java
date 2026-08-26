4e1ba223818489ea89321fe5c800bdf4943c65e6
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-26 19:28:24 +0530

    Time: 1 ms (96.61%), Space: 42.7 MB (36.61%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        recursion(n );
+        return recursion(n) ; 
+    }
+    private static boolean recursion(int n){
+        if(n == 1){
+            return true ; 
+        }
+        if(n <= 0 || n%2 != 0 ){
+            return false ; 
+        }
+        return recursion(n/2);
+    }
+}
